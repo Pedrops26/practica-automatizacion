5642da45f5e323c8226311139d67ca1153b23d1f
Pedrops26 <pedro.saldarriaga@udea.edu.co>
2023-11-15 14:29:12 -0500

    Path for LocatedBy on ITEM_LIST (Amazon Page.java) corrected

--- src/main/java/co/edu/udea/userinterfaces/AmazonPage.java
+++ src/main/java/co/edu/udea/userinterfaces/AmazonPage.java
@@ -9,7 +9,7 @@ public class AmazonPage extends PageObject {
     public static final Target SEARCH_INPUT = the("Search input")
             .locatedBy("//*[@id='twotabsearchtextbox']");
     public static final Target ITEM_LIST = the("First item list")
-            .locatedBy("//*[@id=\"search\"]/div[1]/div[1]/div/span[1]/div[1]/div[4]/div/div/div/div/div/div[1]/div/div[2]/div/span/a");
+            .locatedBy("//*[@id=\"search\"]/div[1]/div[1]/div/span[1]/div[1]/div[5]/div/div/span/div/div/div/div[1]/div/div[2]/div/span/a");
     public static final Target ADD_TO_CART_BUTTON = the("Add to cart button")
             .locatedBy("//*[@id='add-to-cart-button']");
     public static final Target CART_ITEMS_NUMBER = the("Number of item in cart")
